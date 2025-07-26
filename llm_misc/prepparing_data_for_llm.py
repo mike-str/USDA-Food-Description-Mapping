d197917ca3924eab10795034cf5a2cdef119f8b3
mike-str <Michael.P.Strohmeier@gmail.com>
2025-07-26 16:55:24 -0400

    add asa24 experiment 3 and 4

--- llm_misc/prepparing_data_for_llm.py
+++ llm_misc/prepparing_data_for_llm.py
@@ -10,6 +10,8 @@ from util import load_asa, load_nhanes
 if __name__ == "__main__":
     os.makedirs("data/llm/asa24_experiment_1/",  exist_ok=True)
     os.makedirs("data/llm/asa24_experiment_2/",  exist_ok=True)
+    os.makedirs("data/llm/asa24_experiment_3/",  exist_ok=True)
+    os.makedirs("data/llm/asa24_experiment_4/",  exist_ok=True)
     os.makedirs("data/llm/nhanes_experiment_1/", exist_ok=True)
     os.makedirs("data/llm/nhanes_experiment_2/", exist_ok=True)
     os.makedirs("data/llm/nhanes_experiment_3/", exist_ok=True)
@@ -47,6 +49,39 @@ if __name__ == "__main__":
 
     df_target = pd.DataFrame({"target_desc": target_desc_list})
     df_target.to_csv("data/llm/asa24_experiment_2/target_desc_list.csv", index=False)
+
+    # == asa24 experiment 3 ==
+    df = load_asa()
+    df = df[df["input_desc"] != df["target_desc"]]
+    input_desc_list, target_desc_list = df["input_desc"].to_list(), df["target_desc"].to_list()
+    target_desc_list = list(set(target_desc_list))
+    random.shuffle(target_desc_list)
+
+    df_input = pd.DataFrame({"input_desc": input_desc_list})
+    df_input.to_csv("data/llm/asa24_experiment_3/input_desc_list.csv", index=False)
+
+    df_target = pd.DataFrame({"target_desc": target_desc_list})
+    df_target.to_csv("data/llm/asa24_experiment_3/target_desc_list.csv", index=False)
+
+    #  == asa24 experiment 4 ==
+    df = load_asa()
+    df = df[df["input_desc"] != df["target_desc"]]
+    input_desc_list = df["input_desc"].to_list()
+
+    # targets are coming from all of foodb... dropna, select unique, lowercase all
+    target_desc_list = pd.read_csv("data/FooDB_Unique_Descriptions.csv", encoding="latin-1")
+    target_desc_list["orig_food_common_name_uncleaned"] = [s.encode("utf-8").decode("utf-8") for s in target_desc_list["orig_food_common_name_uncleaned"]]
+    target_desc_list = target_desc_list.dropna(subset=["orig_food_common_name_uncleaned"])
+    target_desc_list = list(set(target_desc_list["orig_food_common_name_uncleaned"].to_list()))
+    target_desc_list = [s.lower() for s in target_desc_list]
+    target_desc_list = list(set(target_desc_list))
+    random.shuffle(target_desc_list)
+
+    df_input = pd.DataFrame({"input_desc": input_desc_list})
+    df_input.to_csv("data/llm/asa24_experiment_4/input_desc_list.csv", index=False)
+
+    df_target = pd.DataFrame({"target_desc": target_desc_list})
+    df_target.to_csv("data/llm/asa24_experiment_4/target_desc_list.csv", index=False)
 
     # == nhanes experiment 1 ==
     df = load_nhanes()
